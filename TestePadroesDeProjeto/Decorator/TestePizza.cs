9a02377925dde22ee90c86722a1fc7d10160bbf5
Juliane Pires <juliane.pires@ltm.digital>
2023-04-03 18:11:38 -0300

    Cria testes para os decorators

--- TestePadroesDeProjeto/Decorator/TestePizza.cs
+++ TestePadroesDeProjeto/Decorator/TestePizza.cs
@@ -1,12 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using PadroesDeProjeto.Decorator;
+using PadroesDeProjeto.Decorator.Decorators.Implementacoes;
 
 namespace PadroesDeProjeto.Tests.Decorator
 {
-    internal class TestePizza
+    public class TestePizza
     {
+        [Fact]
+        public void DeveRetornarDescricaoEPrecoPadraoQuandoNaoHouverDecorador()
+        {
+            var valorEsperado = 20;
+            var descricaoEsperada = "Pizza de mussarela";
+            IPizza pizza = new Pizza();
+
+            var valorPizza = pizza.CalculaValor();
+            var descricaoPizza = pizza.PedePizza();
+
+            Assert.Equal(valorEsperado, valorPizza);
+            Assert.Equal(descricaoEsperada, descricaoPizza);
+        }
+
+        [Fact]
+        public void DeveRetornarPropriedadesPadraoAdicionadasAoComportamentoDosDecoradoresConcatenados()
+        {
+            var valorEsperado = 20 + 10 + 14;
+            var descricaoEsperada = "Pizza de mussarela" + " + calabresa" + " + shitake";
+            IPizza pizzaShitake = new PizzaShitake();
+            IPizza pizzaCalabresaShitake = new PizzaCalabresa(pizzaShitake);
+
+            var valorPizza = pizzaCalabresaShitake.CalculaValor();
+            var descricaoPizza = pizzaCalabresaShitake.PedePizza();
+
+            Assert.Equal(valorEsperado, valorPizza);
+            Assert.Equal(descricaoEsperada, descricaoPizza);
+        }
+
+        [Fact]
+        public void DeveRetornarPropriedadesPadraoAdicionadasAoComportamentoDoDecorador()
+        {
+            var valorEsperado = 20 + 10;
+            var descricaoEsperada = "Pizza de mussarela" + " + calabresa";
+            IPizza pizza = new PizzaCalabresa();
+
+            var valorPizza = pizza.CalculaValor();
+            var descricaoPizza = pizza.PedePizza();
+
+            Assert.Equal(valorEsperado, valorPizza);
+            Assert.Equal(descricaoEsperada, descricaoPizza);
+        }
     }
 }
